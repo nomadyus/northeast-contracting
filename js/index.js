9f8e70eaff4f56c19748e00bf2e5650286d428a6
yusuf-kami <yusuf.kami@gmail.com>
2017-02-15 21:45:08 -0500

    adding fade in of msg

--- js/index.js
+++ js/index.js
@@ -4,13 +4,16 @@ function prepareLoadingScreen() {
   console.log("in prepare " + document.readyState);
   hideMainContent();
   showLoader();
+  showComingSoon();
 }
 
 function showLoader() {
   $("#loader").css("display", "");
-  $("#loading-fill").animate({
-    width: '75%'
-  }, 1000);
+  $("#loading-fill").animate({width: '70%'}, 1000);
+}
+
+function showComingSoon() {
+  $("#loading-msg").animate({opacity: '1'}, 1500);
 }
 
 function hideLoader() {
