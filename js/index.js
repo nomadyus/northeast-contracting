57202564b874ddb119322385e049391e6ed9c155
yusuf-kami <yusuf.kami@gmail.com>
2017-02-27 20:35:37 -0500

    slide in header

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-(function(){
+(function () {
   $(document).ready(preparePage);
   var m = false;
 
-function preparePage() {
+  function preparePage() {
     hideMainContent();
     showLoader();
     preLoadImages();
     addEventListenerToElements();
-}
+  }
 
-function preLoadImages() {
+  function preLoadImages() {
     var preLoader = new createjs.LoadQueue();
     preLoader.on("complete", handlePreLoadComplete);
     preLoader.on("progress", handlePreLoadProgress);
     loadImagesToPreloader(preLoader);
-}
+  }
 
-function loadImagesToPreloader(loader) {
+  function loadImagesToPreloader(loader) {
     loader.loadFile("/img/pics/20150307_192018.jpg");
     loader.loadFile("/img/pics/20150307_192056.jpg");
     /*
@@ -47,85 +47,82 @@ function loadImagesToPreloader(loader) {
     loader.loadFile("/img/pics/IMG_4652.JPG");
     loader.loadFile("/img/pics/IMG_4654.JPG");
     */
-}
+  }
 
-function handlePreLoadComplete(event) {
-    console.log("type: " + event.type);
+  function handlePreLoadComplete(event) {
     animateProgressBarTo(100);
     hideLoadingBar();
     slideLoaderOverlayDown();
     slideInHeader();
     showMainContent();
+  }
 
-}
-
-function addEventListenerToElements() {
-    $("#nav-container").click(function(){
+  function addEventListenerToElements() {
+    $("#nav-container").click(function () {
       console.log(m);
-      if(!m) {
-      $("#navigation").addClass("right-0");
-      $("#navigation").removeClass("right--100");
-         $("#nav-container").addClass("menu--opened");
-      $("#nav-container").removeClass("dim");
-      } else{
-      $("#navigation").addClass("right--100");
-      $("#navigation").removeClass("right-0");
-         $("#nav-container").removeClass("menu--opened");
-      $("#nav-container").addClass("dim");
+      if (!m) {
+        $("#navigation").addClass("right-0");
+        $("#navigation").removeClass("right--100");
+        $("#nav-container").addClass("menu--opened");
+        $("#nav-container").removeClass("dim");
+      } else {
+        $("#navigation").addClass("right--100");
+        $("#navigation").removeClass("right-0");
+        $("#nav-container").removeClass("menu--opened");
+        $("#nav-container").addClass("dim");
       }
       m = !m;
     });
-}
+  }
 
-function handlePreLoadProgress(event) {
+  function handlePreLoadProgress(event) {
     animateProgressBarTo(Math.round(event.progress * 100));
-}
+  }
 
-function animateProgressBarTo(percentage) {
+  function animateProgressBarTo(percentage) {
     $("#loading-fill").css({
-        width: percentage + '%'
+      width: percentage + '%'
     });
-}
+  }
 
-function showLoader() {
+  function showLoader() {
     $("#loader").css("display", "");
-}
+  }
 
-function hideLoader() {
+  function hideLoader() {
     $("#loader").css("display", "none");
-}
+  }
 
-function hideLoadingBar() {
+  function hideLoadingBar() {
     $("#loading-logo").addClass("vanish");
     $("#loading-bar").addClass("vanish");
-}
+  }
 
-function hideMainContent() {
+  function hideMainContent() {
     $("#main").css("display", "none");
-}
+  }
 
-function showMainContent() {
+  function showMainContent() {
     $("#main").css("display", "");
     $("#main").removeClass("dn");
     $("#main-content").removeClass("overflow-hidden");
-}
+  }
 
-function slideLoaderOverlayDown() {
+  function slideLoaderOverlayDown() {
     $("#loader").addClass("absolute");
     $("#loader").addClass("slide-down");
-    setTimeout(function() {
-        $("#loader").removeClass("dt");
-        $("#loader").addClass("dn");
-        $("#main-container").removeClass("overflow-hidden");
+    setTimeout(function () {
+      $("#loader").removeClass("dt");
+      $("#loader").addClass("dn");
+      $("#main-container").removeClass("overflow-hidden");
     }, 1750);
-}
+  }
 
-function showScrollArrow() {
+  function showScrollArrow() {
     $("#scroll-arrow").addClass("show");
-}
+  }
 
-function slideInHeader() {
-    $("#header").removeClass("out-of-frame");
-    $("#header").addClass("in-frame");
-}
-           })();
+  function slideInHeader() {
+    $("#header").removeClass("out-of-top-frame");
+  }
+})();
