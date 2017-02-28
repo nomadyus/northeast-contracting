15c60257e27f388f4e3813ea777e1e6604d6fbbb
yusuf-kami <yusuf.kami@gmail.com>
2017-02-28 00:13:13 -0500

    adding images to gallery

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 (function () {
   $(document).ready(preparePage);
+
+  var preLoader = new createjs.LoadQueue();
   var m = false;
 
   function preparePage() {
@@ -10,7 +12,6 @@
   }
 
   function preLoadImages() {
-    var preLoader = new createjs.LoadQueue();
     preLoader.on("complete", handlePreLoadComplete);
     preLoader.on("progress", handlePreLoadProgress);
     loadImagesToPreloader(preLoader);
@@ -19,7 +20,6 @@
   function loadImagesToPreloader(loader) {
     loader.loadFile("/img/pics/20150307_192018.jpg");
     loader.loadFile("/img/pics/20150307_192056.jpg");
-    /*
     loader.loadFile("/img/pics/20150307_192111.jpg");
     loader.loadFile("/img/pics/20150307_192141.jpg");
     loader.loadFile("/img/pics/20150307_192316.jpg");
@@ -27,6 +27,7 @@
     loader.loadFile("/img/pics/20150307_192626.jpg");
     loader.loadFile("/img/pics/20150609_101307.jpg");
     loader.loadFile("/img/pics/20150609_101324.jpg");
+    
     loader.loadFile("/img/pics/20150609_101336.jpg");
     loader.loadFile("/img/pics/20150609_101433.jpg");
     loader.loadFile("/img/pics/20150609_101501.jpg");
@@ -35,6 +36,7 @@
     loader.loadFile("/img/pics/20150609_101552.jpg");
     loader.loadFile("/img/pics/20150609_101612.jpg");
     loader.loadFile("/img/pics/20160527_131702.jpg");
+    /*
     loader.loadFile("/img/pics/IMG_2219.jpg");
     loader.loadFile("/img/pics/IMG_4623.JPG");
     loader.loadFile("/img/pics/IMG_4636.JPG");
@@ -50,11 +52,55 @@
   }
 
   function handlePreLoadComplete(event) {
+
+    loadImagesToGallery();
+    slideImagesToCenter();
+
     animateProgressBarTo(100);
     hideLoadingBar();
     slideLoaderOverlayDown();
     slideInHeader();
     showMainContent();
+  }
+
+  function handlePreLoadProgress(event) {
+    animateProgressBarTo(Math.round(event.progress * 95));
+  }
+
+  function animateProgressBarTo(percentage) {
+    $("#loading-fill").css({
+      width: percentage + '%'
+    });
+  }
+
+  function slideImagesToCenter() {
+    $(".gallery-images").css('right', '0%');
+    $(".gallery-images").css('left', '0%');
+  }
+
+  function loadImagesToGallery() {
+    console.log("gonan do some magic");
+    var items = preLoader.getItems();
+    preLoader.getItems(true).filter(function (item) {
+      var wrap = $("<div class=\"gallery-images slide-horizontally relative dib ma0 pa0 w-50 w-third-m w-25-ns\"></li>");
+      var grid = $("<div class=\"aspect-ratio aspect-ratio--16x9\"></li>");
+      var main = $("<div class=\"db bg-center cover aspect-ratio--object\"></li>");
+      wrap.append(grid.append(main));
+
+      var sqr = Math.sqrt(items.count);
+      var randomInt = Math.round((Math.random() + 1).toFixed(2) * 100);
+      if (randomInt % 2 == 0) {
+        wrap.css('left', -randomInt + '%');
+      } else {
+        wrap.css('right', -randomInt + '%');
+      }
+      var randomDuration = (Math.random() + 0.2).toFixed(2);
+      wrap.attr('data-i', randomInt);
+      wrap.attr('data-d', randomDuration);
+      wrap.css('transition-duration', randomDuration + 's');
+      main.css('background-image', 'url("' + item.result.src + '")');
+      $("#project-gallery").append(wrap);
+    });
   }
 
   function addEventListenerToElements() {
@@ -72,16 +118,6 @@
         $("#nav-container").addClass("dim");
       }
       m = !m;
-    });
-  }
-
-  function handlePreLoadProgress(event) {
-    animateProgressBarTo(Math.round(event.progress * 100));
-  }
-
-  function animateProgressBarTo(percentage) {
-    $("#loading-fill").css({
-      width: percentage + '%'
     });
   }
 
