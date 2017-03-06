ced478162f34d3ff590d340401715ff0d8573106
yusuf-kami <yusuf.kami@gmail.com>
2017-03-05 19:45:32 -0500

    adding p script from mail

--- p/p.php
+++ p/p.php
@@ -1,2 +1,15 @@
 <?php
-echo "Yo";
+if (!(empty($_GET['email']) || empty($_GET['msg']))) {
+  $email = $_GET['email'];
+  $message = $_GET['msg'];
+  $to      = 'hi@yusuffadairo.com';
+  $subject = 'Hello from ' . $email .' at northeastcontracting.ca';
+  $headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+  
+  echo mail($to, $subject, $message, $headers);
+} else {
+  echo false;
+}
+?>
