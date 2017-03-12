be0b2c58b516e49c4dda74f8a1577b224681c490
yusuf-kami <yusuf.kami@gmail.com>
2017-03-12 12:24:59 -0400

    padding mail

--- p/p.php
+++ p/p.php
@@ -2,7 +2,7 @@
 $response = array('sent' => false);
 if (!(empty($_POST['email']) || empty($_POST['message']))) {  
   $email = $_POST['email'];
-  $message = "A visitor has contacted you with the following message:\r\n\n\t". $_POST['message'] . "\r\n\nCheers,\r\nNortheast General Contracting Inc.";
+  $message = "A visitor has contacted you with the following message:\r\n\n". $_POST['message'] . "\r\n\nCheers,\r\nNortheast General Contracting Inc.";
   $to      = 'hi@yusuffadairo.com';
   $subject = 'Hello from ' . $email .' at northeastcontracting.ca';
   $headers = 'From: "Northeast General Contracting Inc." <info@northeastcontracting.ca>' . "\r\n" .
