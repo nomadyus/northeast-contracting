359eedad08138913059b028d8c3300eaa8e2ef22
yusuf-kami <yusuf.kami@gmail.com>
2017-03-12 12:24:11 -0400

    padding mail

--- p/p.php
+++ p/p.php
@@ -1,15 +1,15 @@
 <?php
 $response = array('sent' => false);
-if (!(empty($_POST['email']) || empty($_POST['message']))) {
-  $email = urlencode($_POST['email']);
-  $message = urlencode($_POST['msg']);
+if (!(empty($_POST['email']) || empty($_POST['message']))) {  
+  $email = $_POST['email'];
+  $message = "A visitor has contacted you with the following message:\r\n\n\t". $_POST['message'] . "\r\n\nCheers,\r\nNortheast General Contracting Inc.";
   $to      = 'hi@yusuffadairo.com';
   $subject = 'Hello from ' . $email .' at northeastcontracting.ca';
-  $headers = 'From: info@northeastcontracting.ca' . "\r\n" .
+  $headers = 'From: "Northeast General Contracting Inc." <info@northeastcontracting.ca>' . "\r\n" .
     'Reply-To: info@northeastcontracting.ca' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    'X-Mailer: PHP/' . phpversion() . "\r\n";
   
-  $response['sent'] = mail($to, $subject, $message, $headers);
+  $response['sent'] = mail($to, '=?utf-8?B?'.base64_encode($subject).'?=', $message, $headers);
 }
 echo json_encode($response);
 ?>
