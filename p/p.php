cf23a62cb1a58130b58e1e6563da26f8f97cb169
yusuf-kami <yusuf.kami@gmail.com>
2017-03-14 21:29:31 -0400

    adding actual email address

--- p/p.php
+++ p/p.php
@@ -3,7 +3,7 @@ $response = array('sent' => false);
 if (!(empty($_POST['email']) || empty($_POST['message']))) {  
   $email = $_POST['email'];
   $message = "A visitor has contacted you with the following message:\r\n\n". $_POST['message'] . "\r\n\nCheers,\r\nNortheast General Contracting Inc.";
-  $to      = 'hi@yusuffadairo.com';
+  $to      = 'info@northeastcontracting.ca';
   $subject = 'Hello from ' . $email .' at northeastcontracting.ca';
   $headers = 'From: "Northeast General Contracting Inc." <info@northeastcontracting.ca>' . "\r\n" .
     'Reply-To: info@northeastcontracting.ca' . "\r\n" .
